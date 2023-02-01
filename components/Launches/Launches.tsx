020fce5820f1be2e99d352f5609b0eb8cf2af71b
Joost Meijer <joost.meijer.nl@icloud.com>
2023-02-01 21:25:22 +0100

    Implement states and related logic for loading, errors, and data

--- components/Launches/Launches.tsx
+++ components/Launches/Launches.tsx
@@ -1,13 +1,58 @@
-import Launch from '../Launch';
+import { useEffect, useState } from 'react';
 
-import { launchesTestData } from './Launches.testData';
+import Launch from '../Launch';
+import Button from '../Button';
+
+import { SPACEXDATA_API_V5_LAUNCHES } from './Launches.config';
 
 import styles from './Launches.module.css';
 
 export default function Launches() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [launches, setLaunches] = useState([]);
+
+    useEffect(() => {
+        requestLaunches();
+    }, []);
+
+    const requestLaunches = async () => {
+        setError('');
+        setIsLoading(true);
+
+        try {
+            const response = await fetch(SPACEXDATA_API_V5_LAUNCHES);
+            const data = await response.json();
+            setLaunches(data);
+            setIsLoading(false);
+        } catch (responseError) {
+            setError(responseError.message);
+            setIsLoading(false);
+        }
+    }
+
+    if (error) {
+        return (
+            <div className={ styles.error }>
+                <h2>Oops, something went wrong!</h2>
+                <p>Error: { error }</p>
+                <Button onClick={ requestLaunches }>Try loading again</Button>
+            </div>
+        );
+    }
+
+    if (isLoading) {
+        return (
+            <div className={ styles.loading }>
+                <div className={ styles.loadingSpinner }></div>
+                <p>Loading launch data...</p>
+            </div>
+        );
+    }
+
     return (
-        <div className={styles.grid}>
-            { launchesTestData.slice(0, 10).map((launch, key) => <Launch key={ key } launch={ launch } />) }
+        <div className={ styles.grid }>
+            { launches.slice(0, 10).map((launch, key) => <Launch key={ key } launch={ launch } />) }
         </div>
     );
 }
