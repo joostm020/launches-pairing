c6c67378fa0d6b984deb2c7d5e6a29d549733f7e
Joost Meijer <joost.meijer.nl@icloud.com>
2023-02-01 23:15:47 +0100

    Added await statement to solve unhandled response

--- pages/api/launches.ts
+++ pages/api/launches.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 export default async function handler(req, res) {
-    fetch("https://api.spacexdata.com/v5/launches", {
+    await fetch("https://api.spacexdata.com/v5/launches", {
         method: 'GET',
         redirect: 'follow'
     })
