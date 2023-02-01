8a5a0fd64dac8bc3b0288c2004992e438c076751
Joost Meijer <joost.meijer.nl@icloud.com>
2023-02-01 21:28:07 +0100

    Reformat (whitespaces)

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,34 +5,34 @@ import styles from '../styles/Home.module.css';
 import Launches from '../components/Launches';
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Data SpaceX launches</title>
-        <meta name="description" content="Data SpaceX launches" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div className={styles.container}>
+            <Head>
+                <title>Data SpaceX launches</title>
+                <meta name="description" content="Data SpaceX launches" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-            Data <a href="#">SpaceX</a> launches
-        </h1>
+            <main className={styles.main}>
+                <h1 className={styles.title}>
+                    Data <a href="#">SpaceX</a> launches
+                </h1>
 
-        <Launches/>
-      </main>
+                <Launches />
+            </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
+            <footer className={styles.footer}>
+                <a
+                    href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Powered by{' '}
+                    <span className={styles.logo}>
+                        <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+                    </span>
+                </a>
+            </footer>
+        </div>
+    );
 }
